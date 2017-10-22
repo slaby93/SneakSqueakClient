bcaf440e6b78d555ebbe8868e6c0e8acfb27948d
Daniel Słaby <pufek93@gmail.com>
2017-10-22 19:38:33 +0200

    Moved chunking per component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AsyncComponent from './reactUtils/AsyncComponent'
+import App from './routes/App'
 import store from './store'
 import { Provider } from 'react-redux'
-// https://medium.com/front-end-hacking/lazy-loading-with-react-and-webpack-2-8e9e586cf442
+
 ReactDOM.render(<Provider store={store}>
-  <AsyncComponent moduleProvider={() => import(/* webpackChunkName: "App" */'./routes/App')} />
+  <App />
 </Provider>, document.getElementById('app'))
--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,1 +1,4 @@
-export { default } from './AppContainer'
+import React from 'react';
+import AsyncComponent from './../../reactUtils/AsyncComponent'
+
+export default ()=> <AsyncComponent moduleProvider={() => import(/* webpackChunkName: "App" */'./AppContainer')} />;
