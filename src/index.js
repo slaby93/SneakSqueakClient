bb5cdb70866e9d699cecc50c84aa302577d8919d
Daniel Słaby <pufek93@gmail.com>
2017-10-28 10:27:37 +0200

    Fixed problem with manifest.json. Simple PWA setup finished

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@ import Router from './routes/router'
 ReactDOM.render(<Provider store={store}>
   <Router />
 </Provider>, document.getElementById('app'))
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/serviceworker.js')
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    } catch (err) {
+      console.log('ServiceWorker registration failed: ', err)
+    }
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HappyPack = require('happypack')
-const prodConfig = require('./webpack.config.production')
+const CompressionPlugin = require('compression-webpack-plugin')
 const nodeEnv = JSON.stringify(process.env.NODE_ENV || 'production')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 console.log('Using NODE_ENV:', nodeEnv)
 
 const defaultConfig = {
@@ -47,4 +48,77 @@ const defaultConfig = {
   ]
 }
 
-module.exports = nodeEnv === JSON.stringify('production') ? prodConfig : defaultConfig
+const prodConfig = {
+  context: path.join(__dirname, '/'),
+  devtool: 'inline-source-map',
+  entry: [
+    'babel-polyfill',
+    'whatwg-fetch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/server/public'),
+    chunkFilename: '[name].chunk.js',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'happypack/loader'
+      },
+            { test: /(\.css|\.scss)$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new HappyPack({
+      loaders: ['babel-loader'],
+      threads: 4
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': nodeEnv
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+}
+
+const PWAConfig = {
+  context: path.join(__dirname, '/'),
+  devtool: 'inline-source-map',
+  entry: [
+    './src/serviceworker.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/server/public'),
+    filename: 'serviceworker.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+      [{
+        from: 'src/manifest.json',
+        to: './manifest.json'
+      }]
+    )
+  ]
+}
+
+const configs = [
+  nodeEnv === JSON.stringify('production') ? prodConfig : defaultConfig,
+  PWAConfig
+]
+module.exports = configs
