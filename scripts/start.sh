c78c9220ebff22ae07aed6feb809a070b71757e8
Daniel Słaby <pufek93@gmail.com>
2017-11-04 11:40:25 +0100

    removed scripts that we won't use, updated start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
-#1. create VM's
-docker-machine create --driver virtualbox vm1
-docker-machine create --driver virtualbox vm2
-#2.
-
-# docker-machine ssh myvm1 "docker swarm init --advertise-addr <vm1>"
-# docker-machine ssh myvm1 "docker swarm init --advertise-addr <vm2>"
-
-# docker stack deploy -c docker-compose.yml sneaksqueak
-
+# get parameters
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --start|-s)
+      docker stack deploy -c docker-compose.yml sneaksqueak
+    ;;
+    --stop|-s)
+    docker stack rm sneaksqueak
+    ;;
+  esac
+done
--- scripts/stop-swarm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker service rm web-client-service
-docker swarm leave -f
